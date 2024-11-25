771d363e13d0a470888559b0295e00b5fe639293
Alejandro Cavallo <alejandro.cavallo@gmail.com>
2024-11-25 15:29:33 -0300

    Update authentication tests to handle invalid token scenario and add context usage

--- src/pkgs/droplets/droplets_test.go
+++ src/pkgs/droplets/droplets_test.go
@@ -1,6 +1,7 @@
 package droplets
 
 import (
+	"context"
 	"os"
 	"testing"
 
@@ -12,15 +13,21 @@ func TestAuthenticate(t *testing.T) {
 	t.Run("missing token", func(t *testing.T) {
 		os.Unsetenv("DIGITALOCEAN_ACCESS_TOKEN")
 		client, err := authenticate()
+		
 		assert.Nil(t, client)
 		assert.EqualError(t, err, "the environment variable DIGITALOCEAN_ACCESS_TOKEN is not set")
 	})
 
 	t.Run("valid token", func(t *testing.T) {
-		os.Setenv("DIGITALOCEAN_ACCESS_TOKEN", "valid-token")
+		os.Setenv("DIGITALOCEAN_ACCESS_TOKEN", "invalid-token")
 		client, err := authenticate()
 		assert.NotNil(t, client)
 		assert.NoError(t, err)
 		assert.IsType(t, &godo.Client{}, client)
+		
+		// DO does not validates the token on authentication, it just configures the barer in the client
+		accnt, _, err := client.Account.Get(context.Background())
+		assert.NotNil(t, accnt)
+		assert.NoError(t, err)
 	})
 }
